c9643c11b941379c3c0577fe43ad4b0d1b015093
Paulo Sergio <ngcpaulo1@gmail.com>
2023-04-05 18:26:47 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+// 5
+let pergunta5 = Number (prompt('Digite sua nota'));
+let pergunta55 = Number (prompt('Digite sua nota'));
+let pergunta555 = Number (prompt('Digite sua nota'));
+let pergunta5555 = Number (prompt('Digite sua nota'));
+console.log('A soma das notas:', pergunta5 + pergunta55 + pergunta555 + pergunta5555);
+console.log('Sua média:', (pergunta5 + pergunta55 + pergunta555 + pergunta5555) / 4); 
+
